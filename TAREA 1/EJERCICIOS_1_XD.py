6069fca38e0c9e2238c8a7116edcd9f034ed8a8d
Jonh28-02 <jriverag1900@alumno.ipn.mx>
2021-03-07 21:16:24 -0600

    Tarea 1 FINALIZADA

--- TAREA 1/EJERCICIOS_1_XD.py
+++ TAREA 1/EJERCICIOS_1_XD.py
@@ -14,6 +14,8 @@ MENU
 10. Ejercicio 10
 11. Salir
 """
+
+
 def Ejercicio_1():
      sueldo=int(input("Ingresa tu salario"))
      if sueldo < 4000:
@@ -21,63 +23,115 @@ def Ejercicio_1():
      else:
          print((sueldo * .08)+sueldo)
          
+         
+         
 
 def Ejercicio_2():
      edad=int(input("Ingresa tu edad"))
      if edad < 10:
           print("Tu monto a pagar es de: "(50*.75), "soles")
+          
      else:
           print("Tu monto a pagar es de 50 soles")
 
 
 def Ejercicio_3():
-  print("xd")
+     mes=int(input("Ingresa el numero de mes "))
+     monto=int(input("Ingrese el monto "))
+
+     if mes < 2 or mes > 2:
+          print("Tu monto a pagar es de: ", monto)
+     else:
+          print("Tu monto a pagar es de: ", monto * .75)
+
 
 def Ejercicio_4():
-  print("xd")
+     num=int(input("Ingresa un numero"))
+     while num >= 10:
+      num=int(input("Ingresa un numero"))
+     if num < 10:
+          print("El numero es: ", num)
+               
 
 def Ejercicio_5():
-  print("xd")
+     num=int(input("Ingresa un numero"))
+     while num <= 0 or num >= 20:
+      num=int(input("Ingresa un numero"))
+     if num > 0 and num < 20:
+        print("El numero es: ", num)
+     
 
 def Ejercicio_6():
-  print("xd")
-
+     num=int(input("Ingresa un numero: "))
+     nv=1
+     while num <= 0 or num >= 20:
+      num=int(input("Ingresa un numero: "))
+      nv = nv+1
+     if num > 0 and num < 20:
+        print("El numero es: ", num)
+     print("Ingresaste el numero ",nv , " veces")
+     
 def Ejercicio_7():
 
-  print("xd")
-def Ejercicio_8():
-  print("xd")
+     suma = 0
+     numero = int(input("Ingrese un número: "))
+     while numero>0:
+       suma = (numero*(numero+1))/2
+       break
+     print ("La suma total es de :",suma)
+
 
 def Ejercicio_9():
-  print("xd")
+     suma = 0
+     while suma < 100:
+          num = int(input("Ingrese un numero: "))
+          suma = suma + num
+     print("La suma es de: ", suma)  
+
+def Ejercicio_8():
+     suma = 0
+     print("Si deseas detener la suma ingresa 0")
+     num = int(input("Ingrese un numero para confirmar lo entendido: "))
+     while num < 0 or num > 0:
+          num = int(input("Ingrese un numero: "))
+          suma = suma + num
+     # print("La suma es de: ", suma)
 
 def Ejercicio_10():
-  print("xd")
+     nombre=input("Ingresa tu nombre: ")
+     NH=int(input("Ingresa el numero de hijos: "))
+     HT=int(input("Ingresa las horas trabajadas: "))
+     HE=int(input("Ingresa las horas extra trabajadas: "))
+     PH=int(input("Ingresa el pago por hora: "))
+
+     print("El trabajador ",nombre)
+     print("Tiene un monto de horas normales igual a: ",HT*PH,"un monto por horas extra de: ",HE*(PH*1.5),"y una bonificacion de: ", NH*.5)
+     print("El pago total sera de: ", ((HT*PH)+(HE*(PH*1.5)))*(NH*.5))
 
 while True:
 
      print(menu)
-     op=int(input("Ingrese la opcion que desee xd:"))
+     op=int(input("Ingrese la opcion que desee xd: "))
 
-     if op is 1:
+     if op == 1:
           Ejercicio_1()
-     elif op is 2:
+     elif op == 2:
           Ejercicio_2()
-     elif op is 3:
+     elif op == 3:
           Ejercicio_3()
-     elif op is 4:
+     elif op == 4:
           Ejercicio_4()
-     elif op is 5:
+     elif op == 5:
           Ejercicio_5()
-     elif op is 6:
+     elif op == 6:
           Ejercicio_6()
-     elif op is 7:
+     elif op == 7:
           Ejercicio_7()
-     elif op is 8:
+     elif op == 8:
           Ejercicio_8()
-     elif op is 9:
+     elif op == 9:
           Ejercicio_9()
-     elif op is 10:
+     elif op == 10:
           Ejercicio_10()
-     elif op is 11:
+     elif op == 11:
           break
